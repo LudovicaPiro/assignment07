0f78f193a51f800c66c198b52d5b47916fe59d05
LudovicaPiro <32323247+LudovicaPiro@users.noreply.github.com>
2018-02-01 18:39:42 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,100 +1,121 @@
 
-
-var fillColor;
+var agg=1;
+var delta=2;
 var mic;
-
-function setup() {	createCanvas(500,500);
+var a=0;
+var vivo=true;
+function setup() {  createCanvas(500,500);
                   
                   //immagini
-					capture= createCapture(VIDEO);
-					capture.size(500,500);
-					capture.hide();
+                    capture= createCapture(VIDEO);
+                    capture.size(500,500);
+                    capture.hide();
                   
                   //microfono
                     mic = new p5.AudioIn();
                     mic.start();
                     
                     angleMode(DEGREES);     
-				}
+                }
 
-function draw() {	
-					 background("#73c2fb");
-    //collina
-    fill("#014421")
-    ellipse(250,500, 700, 220);
-                  // sole  
+function draw() {    var vol=mic.getLevel();
+                     background("#73c2fb");
+                    
+                    //collina
+                    fill("#014421")
+                    ellipse(250,500, 700, 220);
+    
+                    // sole 
                     fill("#ffc40c")
                     ellipse(0,0, 190,190);
-                    
-                    var vol=mic.getLevel();
-                    if(vol>0.03 && vol<0.1) {  // var Ya=350-a;
-                                   // var yB=500+a;
-                                    var a=0;
-                                    a+=10 +frameCount/10
-                                }
-                  
-                    else{   //var yA=350;
-                            //  var yB=500;
-                         fill("#ffc40c")
-                    ellipse(0,0, 190,190);
-                            a=0;
+       
+                    //crescita dello stelo
+                    if(vol<0.04 && vivo==true &&vol>=0.003) {    
+                                        a+=0.2
+                                    }
+                    else if(vol>=0.04) {vivo=false;
+                                        a+=0;}
+                    else{   
+                           // ellipse(0,0, 190,190);
+                            //a=0;
                         };
-                    
-                    translate(0,-a);
-    //stelo(yA, yB);
-                    stelo("#d1e231");
+                 //colore stelo
+              if (vol>=0.04 ||vivo==false) 
+                  
+                    {       stelo("#bc8f8f");
+                            push();
+                            translate(280,350-a);
+
+                            push();
+                            rotate(60);
+                            fiore("#856979");
+                            pop();
+
+                            translate(-45,0);
+                            rotate(-60);
+                            fiore("#856979");
+                            pop();
+                    }
+                 
+                 
+                else {  stelo("#d1e231"); 
+                                //fiori
+                            push();
+                            translate(280,350-a);
+
+                            push();
+                            rotate(60);
+                            fiore("#ffc40c");
+                            pop();
+
+                            translate(-45,0);
+                            rotate(-60);
+                            fiore("#ffc40c");
+                            pop();
+                    }
+                           
+                  if(a==delta){                delta+=2;
+                                             for(i=0; i<=agg; i+=10) {agg+=1
+                                                ellipse(100, 300-i, 30,30);}
+                                         }
+  
+    var level = mic.getLevel();
+
     
-        //fiori
-                    push();
-                    translate(280,350);
+                    fill(255,255,255);
+                    rect(20,20,300,50);
+                    fill(0,0,0);
+                    text(level, 20,40);
+                    text(a, 20,60);
+                    text(delta, 20, 70)
+    
+                }
 
-                    push();
-                    rotate(60);
-					fiore();
-                    pop();
+function fiore(nettare) {   var vol=mic.getLevel();
+                            if (vol>=0.04 ||vivo==false){var p="#d18e8e";}
+                    else{   var myImage=capture.loadPixels();
+                            var p=myImage.get(250,250);
+                        }
+                            noStroke();
+               /*     
+                     var myImage=capture.loadPixels();
+                     var p=myImage.get(250,250);
+                      */
 
-                    translate(-45,0);
-                    rotate(-60);
-                    fiore();
-                    pop();
-                    
-                   
-                    
-//foglie            
-  /*                  noStroke();
-                    fill("#d1e231");
-                    arc(255, 400, 110, 110, 0,60, PIE);
-
-                    push();
-                    translate(260,400)
-                    rotate(120);
-                    arc(0, 0, 110, 110, 0,60, PIE);
-                    pop();
-*/
-				}
-
-function fiore() {	
-                    noStroke();
-                    //fill("#ffcff1");
-                    var myImage=capture.loadPixels();
-                    var p=myImage.get(250,250);
                     fill(p);
-					ellipse(250-250,0, 45,50);
+                    ellipse(250-250,0, 45,50);
 
-					fill("#73c2fb");
-					triangle(250-250, 255-250, 210-250, 190-250, 290-250, 190-250);
-
-                    fill("#ffc40c");
-                    ellipse (250-250,250-250, 10, 10);
+                    fill("#73c2fb");
+                    triangle(250-250, 255-250, 210-250, 190-250, 290-250, 190-250);
+            //nettare
+                    fill(nettare); //giallo
+                    ellipse (0,0, 10, 10);
 
                  }
-
-function stelo(col) {  
+function stelo(col) {   
                         noStroke();
                         fill(col);
-                 //   rect(252, yA,10,yB);
-                        rect(252,350,10, 500);
-                        
+                        rect(252,350-a,10, 500+a);     
                     arc(255, 400, 110, 110, 0,60, PIE);
 
                     push();
